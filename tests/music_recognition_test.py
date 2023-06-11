98bd9205a394efab99c002f51c85d1aae8e47535
Shaked Ben Baruch <shakos123@gmail.com>
2023-06-11 16:06:11 +0300

    test: fixed test + added existing famous track test

--- tests/music_recognition_test.py
+++ tests/music_recognition_test.py
@@ -1,8 +1,7 @@
-import pytest
 import os
 from music_recognition import recognize
 
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'media')
 
 
 def test_existing_track_wav():
@@ -18,4 +17,8 @@ def test_non_existing_track_aac():
 
 
 def test_non_existing_famous_track():
-    assert not recognize(os.path.join(DIR_PATH, 'Billie_Jean_sample.wav'))
+    assert not recognize(os.path.join(DIR_PATH, 'Space_Oddity_sample.wav'))
+
+
+def test_existing_famous_track():
+    assert recognize(os.path.join(DIR_PATH, 'Billie_Jean_sample.wav'))

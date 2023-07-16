77a7a8c3a7fb94f0c386c694a0f3038612fe9f7c
Shaked Ben Baruch <shakos123@gmail.com>
2023-07-16 18:08:02 +0300

    feat: Audio urls from location ID
    Added method for getting urls from location ID

--- instagram_bot.py
+++ instagram_bot.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import xmltodict
 from moviepy.editor import VideoFileClip
 from dotenv.main import load_dotenv
 load_dotenv()
@@ -43,6 +44,14 @@ class IGBOT:
         return response.json()['id']
 
     @staticmethod
+    def convert_story_videos_to_audio():
+        for story_video in [os.path.join(STORIES_DIR_PATH, path) for path in os.listdir(STORIES_DIR_PATH)]:
+            file_path, file_extension = os.path.splitext(story_video)
+            if file_extension == ".mp4":
+                video = VideoFileClip(story_video)
+                video.audio.write_audiofile(f"{file_path}.mp3")
+
+    @staticmethod
     def download_user_stories(user_id: str) -> dict:
         stories = IGBOT.download_user_stories_as_videos(user_id)
         IGBOT.convert_story_videos_to_audio()
@@ -76,9 +85,32 @@ class IGBOT:
             raise IGDownloadError(response.text)
 
     @staticmethod
-    def convert_story_videos_to_audio():
-        for story_video in [os.path.join(STORIES_DIR_PATH, path) for path in os.listdir(STORIES_DIR_PATH)]:
-            file_path, file_extension = os.path.splitext(story_video)
-            if file_extension == ".mp4":
-                video = VideoFileClip(story_video)
-                video.audio.write_audiofile(f"{file_path}.mp3")
+    def get_audio_urls_from_post_location_id(location_id: int) -> dict:
+        """Get usernames and their audio URL of recent Instagram Posts in entered location"""
+        url = "https://instagram-scraper-2022.p.rapidapi.com/ig/locations/"
+        querystring = {"location_id": location_id}
+        headers = {
+            "X-RapidAPI-Key": os.environ.get("X_RAPID_API_KEY"),
+            "X-RapidAPI-Host": os.environ.get("X_RAPID_API_HOST")
+        }
+        response = requests.get(url, headers=headers, params=querystring)
+
+        if response.ok:
+            sections = response.json()['native_location_data']['recent']['sections']
+            location_audios = dict()
+            for section in sections:
+                medias = section['layout_content']['medias']
+                for media in [media for media in medias if media['media'].get('video_dash_manifest')]:
+                    xml_data = xmltodict.parse(media['media']['video_dash_manifest'])
+                    audio_represent_url = xml_data['MPD']['Period']['AdaptationSet'][1]['Representation']['BaseURL']
+                    if type(audio_represent_url) is str:
+                        audio_url = audio_represent_url
+                    else:
+                        audio_url = audio_represent_url['#text']
+                    if not location_audios.get(media['media']['user']['username']):
+                        location_audios[media['media']['user']['username']] = []
+                    location_audios[media['media']['user']['username']].append(audio_url)
+
+            return location_audios
+        else:
+            raise IGGetError(response.text)

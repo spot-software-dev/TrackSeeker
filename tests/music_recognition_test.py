9de88485b5cc0ab688a827374a8e8fcda76094ea
Shaked Ben Baruch <shakos123@gmail.com>
2023-07-18 23:06:40 +0300

    test: Changed unittest to Pytest
    Changed test library from unittest to Pytest for cleaner code and ease of use.

--- tests/music_recognition_test.py
+++ tests/music_recognition_test.py
@@ -1,23 +1,21 @@
-import unittest
+import pytest
 import os
 from music_recognition import recognize
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-class MyTestCase(unittest.TestCase):
-    def test_existing_track_wav(self):
-        self.assertEqual(recognize(os.path.join(DIR_PATH, 'red_samba_sample.wav')), True)
-
-    def test_existing_track_aac(self):
-        self.assertEqual(recognize(os.path.join(DIR_PATH, 'red_samba_sample.aac')), True)
-
-    def test_non_existing_track_aac(self):
-        self.assertEqual(recognize(os.path.join(DIR_PATH, 'Panoramaxx_sample.aac')), False)
-
-    def test_non_existing_famous_track(self):
-        self.assertEqual(recognize(os.path.join(DIR_PATH, 'Billie_Jean_sample.wav')), False)
+def test_existing_track_wav():
+    assert recognize(os.path.join(DIR_PATH, 'red_samba_sample.wav'))
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_existing_track_aac():
+    assert recognize(os.path.join(DIR_PATH, 'red_samba_sample.aac'))
+
+
+def test_non_existing_track_aac():
+    assert not recognize(os.path.join(DIR_PATH, 'Panoramaxx_sample.aac'))
+
+
+def test_non_existing_famous_track():
+    assert not recognize(os.path.join(DIR_PATH, 'Billie_Jean_sample.wav'))

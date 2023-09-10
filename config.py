549bf80241acbed3569d8faac336c0c676b4900e
Shahar Yair <shaharyair12@gmail.com>
2023-09-10 19:26:15 +0300

    ref: move flask config to config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+class Config:
+    # Flask Mail Configuration
+    MAIL_SERVER = os.environ.get('EMAIL_HOST')  
+    MAIL_PORT = int(os.environ.get('EMAIL_PORT'))  
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('EMAIL_ADDRESS')  
+
+    # CORS Configuration
+    CORS_HEADERS = 'Content-Type'

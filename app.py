f3ceea27b638d99d74eb6bed57815545c2db23cd
Shaked Ben Baruch <shakos123@gmail.com>
2023-10-01 13:30:12 +0300

    fix: get locations security issue
    Changed get locations GET request to POST request in order to avoid security issues

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def get_location_songs():
         return jsonify(error=str(e)), 500
 
 
-@app.route('/api/locations', methods=['GET'])
+@app.route('/api/locations', methods=['POST'])
 def get_locations():
     data = request.get_json()
     dashboard = data.get("dashboard")

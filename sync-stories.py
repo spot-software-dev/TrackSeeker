9167a0fbda22605e72b4c2fd981753ad7801c2ae
Shaked Ben Baruch <shakos123@gmail.com>
2023-10-26 17:13:50 +0300

    fix: Drive current date update
    Drive class is instanced once, if used overnight (as intended using AWS server) it will result in checking the same date over and over again without updating to current date.
    Added date update each sync.

--- sync-stories.py
+++ sync-stories.py
@@ -1,15 +1,18 @@
 from loguru import logger
-from datetime import datetime
+import datetime
 from drive_logic import Drive
 
 if __name__ == "__main__":
     drive = Drive()
     while True:
-        logger.info(f'Synchronizing at {datetime.now()}')
+        date_now = datetime.datetime.today().date()
+        Drive.date_now = date_now
+        logger.info(f'Synchronizing at {date_now}')
         try:
             from logic import master_sync
             master_sync(drive)
         except Exception as e:
             logger.error(f'Error occurred: {e}')
         else:
-            logger.success(f'Completed sync with no errors at {datetime.now()}')
+            logger.success(f'Completed sync with no errors at {date_now}')
+
